c2772c0244ba270053651b243ed997208b7dc527
Ricardo Daniel <Ricperez0101@gmail.com>
2021-10-08 17:24:19 -0500

    Webpack

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import fslightbox from 'fslightbox';
+
+
+/* import {suma, saludo} from "./operaciones";
+import Swal from 'sweetalert2'
+
+ document.write(`<h1>${suma(1, 2)}</h1>`)
+
+console.log(suma(1, 2));
+console.log(saludo);
+console.log(suma(10, 3)); */
+
+/* Swal.fire({
+    title: 'Error!',
+    text: 'Do you want to continue',
+    icon: 'error',
+    confirmButtonText: 'Cool'
+  }) */
+
+/*   Swal.fire({
+    title: 'هل تريد الاستمرار؟ <br> Ricardo Daniel Alavarez Perez ',
+    icon: 'question',
+    iconHtml: '؟',
+    confirmButtonText: 'نعم',
+    cancelButtonText: 'لا',
+    showCancelButton: true,
+    showCloseButton: true
+  }) */
+
+/* import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+   */
+  
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import "fslightbox";
+
+/* fsLightboxInstances['first-lightbox'].open(0);
+fsLightboxInstances['second-lightbox'].props.onOpen = () => console.log('Lightbox open!'); */
+
+
+/* import {suma} from "./operaciones";
+
+/* document.write(`<h2>${suma(1, 2)}</h2>`) */
+/* console.log(suma(1, 2));
+console.log(saludo); */
